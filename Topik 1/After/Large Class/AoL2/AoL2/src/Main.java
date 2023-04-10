35143aaf7dd1e597355aac815d12d2dbea3fa861
KantaTatstumi <66326252+KantaTatstumi@users.noreply.github.com>
2023-04-10 16:20:07 +0700

    Add files via upload

---
+++ Topik 1/After/Large Class/AoL2/AoL2/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Customer cus = new Customer("Adit", 10, 20);
+		menu.printDetails(cus);
+        
+	}
+
+}
---
+++ Topik 1/After/Large Class/AoL2/AoL2/src/menu.java
@@ -0,0 +1,8 @@
+public class menu {
+    
+    public static void printDetails(Customer cus) {
+        System.out.println("Name: " + cus.getName());
+        System.out.println("Age: " + cus.getAge());
+        System.out.println("Credit: " + cus.getCredit());
+    }
+}
