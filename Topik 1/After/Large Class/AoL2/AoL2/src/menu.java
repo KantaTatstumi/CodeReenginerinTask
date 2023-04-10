27b74ff105b52b2b5632295d012dde1c87791bb1
KantaTatstumi <66326252+KantaTatstumi@users.noreply.github.com>
2023-04-11 01:02:05 +0700

    Delete Topik 1 directory

--- Topik 1/After/Large Class/AoL2/AoL2/src/menu.java
+++
@@ -1,8 +0,0 @@
-public class menu {
-    
-    public static void printDetails(Customer cus) {
-        System.out.println("Name: " + cus.getName());
-        System.out.println("Age: " + cus.getAge());
-        System.out.println("Credit: " + cus.getCredit());
-    }
-}
